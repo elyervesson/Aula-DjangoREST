d533c4931fd7a458bc5e2c4618f00aba616e02c4
bruno-rocha <bruno.rocha@ccc.ufcg.edu.br>
2016-03-22 15:55:11 -0300

    Aula 4 Completa

--- rest/projeto/snippets/serializers.py
+++ rest/projeto/snippets/serializers.py
@@ -1,47 +1,57 @@
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from django.contrib.auth.models import User
 
-"""
-class SnippetSerializer(serializers.Serializer):
-    pk = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-    code = serializers.CharField(style={'base_template': 'textarea.html'})
-    linenos = serializers.BooleanField(required=False)
-    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
-    style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
-
-    def create(self, validated_data):
-
-        #Create and return a new `Snippet` instance, given the validated data.
-
-        return Snippet.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        
-        #Update and return an existing `Snippet` instance, given the validated data.
-        
-        instance.title = validated_data.get('title', instance.title)
-        instance.code = validated_data.get('code', instance.code)
-        instance.linenos = validated_data.get('linenos', instance.linenos)
-        instance.language = validated_data.get('language', instance.language)
-        instance.style = validated_data.get('style', instance.style)
-        instance.save()
-        return instance
-
-The first part of the serializer class defines the fields that get 
-serialized/deserialized. The create() and update() methods define 
-how fully fledged instances are created or modified when calling 
-serializer.save()
-"""
+# """
+# class SnippetSerializer(serializers.Serializer):
+#     pk = serializers.IntegerField(read_only=True)
+#     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+#     code = serializers.CharField(style={'base_template': 'textarea.html'})
+#     linenos = serializers.BooleanField(required=False)
+#     language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
+#     style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+#
+#     def create(self, validated_data):
+#
+#         #Create and return a new `Snippet` instance, given the validated data.
+#
+#         return Snippet.objects.create(**validated_data)
+#
+#     def update(self, instance, validated_data):
+#
+#         #Update and return an existing `Snippet` instance, given the validated data.
+#
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.code = validated_data.get('code', instance.code)
+#         instance.linenos = validated_data.get('linenos', instance.linenos)
+#         instance.language = validated_data.get('language', instance.language)
+#         instance.style = validated_data.get('style', instance.style)
+#         instance.save()
+#         return instance
+#
+# The first part of the serializer class defines the fields that get
+# serialized/deserialized. The create() and update() methods define
+# how fully fledged instances are created or modified when calling
+# serializer.save()
+# """
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style', 'owner')
 
-"""
-It's important to remember that ModelSerializer classes don't do anything particularly magical, they are simply a shortcut for creating serializer classes:
+# """
+# It's important to remember that ModelSerializer classes don't do anything particularly magical, they are simply a shortcut for creating serializer classes:
+#
+# An automatically determined set of fields.
+# Simple default implementations for the create() and update() methods.
+# """
 
-An automatically determined set of fields.
-Simple default implementations for the create() and update() methods.
-"""
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'snippets')
